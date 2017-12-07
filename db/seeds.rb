23d39a405f566a8468a339fc442e271b21b40a2c
mrbernnz <noel.sagaille@gmail.com>
2017-12-07 15:05:04 -0500

    * Config documents api

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |d|
+  FreelanceDocument.create!(
+    title: "Document #{d + 1}",
+    description: 'Lorem ipsum dolor sit amet.',
+    file_url: 'https://drive.google.com/file/d/0B09nzZwGw3nCcTdTUDRnSG1RSGEwLU9JTDVsUmhDbGZxdnRV/view?usp=sharing',
+    image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg/1280px-Mistakes-to-avoid-when-hiring-freelancers.jpg',
+  )
+end
